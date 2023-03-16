dee021d00597fad9d52b2b5f5867f9715950ad6b
Quang Tran <ttvquang@gmail.com>
2023-03-16 18:10:34 +0700

    Complete part 1

--- src/getMessageFromDevin.js
+++ src/getMessageFromDevin.js
@@ -1,8 +1,43 @@
 const { httpGet } = require("./mockHttpService");
 
+// const getMessageFromQuang = urls.map(async (url) => {
+//   console.log('')
+
+// }
+// )
+
 const getMessageFromDevin = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  let result = [];
+  
+  for (let i=0; i<urls.length; i++) {
+
+    let response = await httpGet(urls[i]);
+    // let response =  httpGet(urls[i]);
+    let oResponseBody = JSON.parse(response.body);
+    let newObject = {};
+    
+    // console.log('response value: ' + Object.values(oResponseBody).toString());    
+    // console.log('response value type: ' + typeof(Object.values(oResponseBody).toString()));    
+  
+    if (response.status === 200) {
+      // console.log('====== Status code = 200 ======')
+      newObject['Devin Nguyen'] = Object.values(oResponseBody).toString();
+      // console.log('New Object key: ' + Object.keys(newObject));
+      // console.log('New Object value: ' + Object.values(newObject[0]));
+      // console.log('New Object value: ' + Object.values(newObject)[0]);
+      // console.log('New Object value data type: ' + typeof(Object.values(newObject)[0]));
+    }
+    
+    if (response.status !== 200) {
+      // console.log('======= Status code is not equal 200 =======')
+      newObject['FAILED'] = Object.values(oResponseBody).toString();
+    }   
+
+    result[i] = newObject;
+  } 
+
+  return result;
+
 };
 
 module.exports = {
--- src/getMessageFromQuang.js
+++ src/getMessageFromQuang.js
@@ -1,8 +1,10 @@
 const { httpGet } = require("./mockHttpService");
 
-// const urlPrefix = `http://www.devinnguyen.vn`;
-// let url = ['http://www.devinnguyen.vn/devin0']
+// const getMessageFromQuang = urls.map(async (url) => {
+//   console.log('')
 
+// }
+// )
 
 const getMessageFromDevin = async (urls) => {
   let result = [];
@@ -10,14 +12,15 @@ const getMessageFromDevin = async (urls) => {
   for (let i=0; i<urls.length; i++) {
 
     let response = await httpGet(urls[i]);
+    // let response =  httpGet(urls[i]);
     let oResponseBody = JSON.parse(response.body);
     let newObject = {};
     
-    console.log('response value: ' + Object.values(oResponseBody).toString());    
-    console.log('response value type: ' + typeof(Object.values(oResponseBody).toString()));    
+    // console.log('response value: ' + Object.values(oResponseBody).toString());    
+    // console.log('response value type: ' + typeof(Object.values(oResponseBody).toString()));    
   
     if (response.status === 200) {
-      console.log('====== Status code = 200 ======')
+      // console.log('====== Status code = 200 ======')
       newObject['Devin Nguyen'] = Object.values(oResponseBody).toString();
       // console.log('New Object key: ' + Object.keys(newObject));
       // console.log('New Object value: ' + Object.values(newObject[0]));
@@ -26,7 +29,7 @@ const getMessageFromDevin = async (urls) => {
     }
     
     if (response.status !== 200) {
-      console.log('====== Status code is not equal 200 ======')
+      // console.log('======= Status code is not equal 200 =======')
       newObject['FAILED'] = Object.values(oResponseBody).toString();
     }   
 
